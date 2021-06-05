aeb79205eb18e82400adb46abc901bfd2635a227
Faisal kalwar <faisalkalwar48@gmail.com>
2021-06-05 21:41:43 +0500

    JS Coding TASKS 5

---
+++ JS Coding TASKS 5/FunctionWithMultipleInputParameters/app.js
@@ -0,0 +1,10 @@
+// function AddTwoNumbers(Num1 , Num2){
+//     let sum = Num1 + Num2;
+//     return sum;
+// }
+// AddTwoNumbers(20 , 17);
+
+function AddTwoNumbers(Num1 , Num2){
+    return Num1 + Num2;
+}
+console.log(AddTwoNumbers(2, 5));
---
+++ JS Coding TASKS 5/IntroductionToFunction/app.js
@@ -0,0 +1,6 @@
+function double(paisa){
+    let answer = paisa * 2;
+    return answer;
+}
+console.log(double(5));
+console.log(double(65));

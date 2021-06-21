37fc0475c9b469eaff532b6c5298b4fac699d8aa
Faisal kalwar <faisalkalwar48@gmail.com>
2021-06-21 15:22:45 +0500

    C17JS task completed

---
+++ C17 - TASKS/app.js
@@ -0,0 +1,49 @@
+// 1
+let myName = "Faisal";
+console.log(myName);
+// Output Faisal
+/* let myName = "Faisal";
+ console.log(myName);*/
+//  output nothing
+
+
+// 2
+let my_num = 3;
+console.log(typeof my_num);
+let my_num = 3.5;
+console.log(typeof my_num);
+/* Output number
+Output number 
+no difference in the data types
+*/
+
+// 3
+let my_bool = true;
+console.log(typeof my_bool);
+// boolean
+my_bool = "sir saeed";
+console.log(typeof my_bool);
+// string
+
+// 4
+let my_num = 3;
+console.log(typeof my_num);
+let my_bool = true;
+console.log(typeof my_bool);
+/*
+Output number
+Outut boolean
+*/
+
+// 5
+let myName = "Faisal";
+console.log(myName);
+//Output: Faisal
+myName = Saeed;
+console.log(myName)
+//Output: VM246:1 Uncaught ReferenceError: Saeed is not defined
+
+// 6
+let x = null;
+console.log(typeof x);
+//Output: object
